730ff574a1952f0aec2832a48c01b8564445d708
arimpact <mingdash@gmail.com>
2015-09-06 12:13:04 -0700

    Update engine.js

--- js/engine.js
+++ js/engine.js
@@ -1,34 +1,5 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-/* Engine.js
- * This file provides the game loop functionality (update entities and render),
- * draws the initial game board on the screen, and then calls the update and
- * render methods on your player and enemy objects (defined in your app.js).
- *
- * A game engine works by drawing the entire game screen over and over, kind of
- * like a flipbook you may have created as a kid. When your player moves across
- * the screen, it may look like just that image/character is moving or being
- * drawn but that is not the case. What's really happening is the entire "scene"
- * is being drawn over and over, presenting the illusion of animation.
- *
- * This engine is available globally via the Engine variable and it also makes
- * the canvas' context (ctx) object globally available to make writing app.js
- * a little simpler to work with.
- */
-
-var Engine = (function(global) {
-    /* Predefine the variables we'll be using within this scope,
-     * create the canvas element, grab the 2D context for that canvas
-     * set the canvas elements height/width and add it to the DOM.
-     */
-=======
 var Engine = (function(global) {
     //predefined global variables
->>>>>>> master
-=======
-var Engine = (function(global) {
-    //predefined global variables
->>>>>>> master
     var doc = global.document,
         win = global.window,
         canvas = doc.createElement('canvas'),
@@ -43,59 +14,6 @@ var Engine = (function(global) {
      * and handles properly calling the update and render methods.
      */
     function main() {
-<<<<<<< HEAD
-<<<<<<< HEAD
-        /* Get our time delta information which is required if your game
-         * requires smooth animation. Because everyone's computer processes
-         * instructions at different speeds we need a constant value that
-         * would be the same for everyone (regardless of how fast their
-         * computer is) - hurray time!
-         */
-        var now = Date.now(),
-            dt = (now - lastTime) / 1000.0;
-
-        /* Call our update/render functions, pass along the time delta to
-         * our update function since it may be used for smooth animation.
-         */
-        update(dt);
-        render();
-
-        /* Set our lastTime variable which is used to determine the time delta
-         * for the next time this function is called.
-         */
-        lastTime = now;
-
-        /* Use the browser's requestAnimationFrame function to call this
-         * function again as soon as the browser is able to draw another frame.
-         */
-        win.requestAnimationFrame(main);
-    }
-
-    /* This function does some initial setup that should only occur once,
-     * particularly setting the lastTime variable that is required for the
-     * game loop.
-     */
-    function init() {
-        reset();
-        lastTime = Date.now();
-        main();
-    }
-
-    /* This function is called by main (our game loop) and itself calls all
-     * of the functions which may need to update entity's data. Based on how
-     * you implement your collision detection (when two entities occupy the
-     * same space, for instance when your character should die), you may find
-     * the need to add an additional function call here. For now, we've left
-     * it commented out - you may or may not want to implement this
-     * functionality this way (you could just implement collision detection
-     * on the entities themselves within your app.js file).
-     */
-    function update(dt) {
-        updateEntities(dt);
-        // checkCollisions();
-=======
-=======
->>>>>>> master
 
         //Get time delta information which is required for movement animation
 
@@ -219,10 +137,6 @@ var Engine = (function(global) {
     function update(dt) {
         updateEntities(dt);
         player.checkCollisions();
-<<<<<<< HEAD
->>>>>>> master
-=======
->>>>>>> master
     }
 
     /* This is called by the update function  and loops through all of the
@@ -277,28 +191,12 @@ var Engine = (function(global) {
                 ctx.drawImage(Resources.get(rowImages[row]), col * 101, row * 83);
             }
         }
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-
-=======
->>>>>>> master
-=======
->>>>>>> master
         renderEntities();
     }
 
     /* This function is called by the render function and is called on each game
      * tick. It's purpose is to then call the render functions you have defined
-<<<<<<< HEAD
-<<<<<<< HEAD
-     * on your enemy and player entities within app.js
-=======
      * on your enemy, player, gem and "NICE" entities within app.js
->>>>>>> master
-=======
-     * on your enemy, player, gem and "NICE" entities within app.js
->>>>>>> master
      */
     function renderEntities() {
         /* Loop through all of the objects within the allEnemies array and call
@@ -307,31 +205,11 @@ var Engine = (function(global) {
         allEnemies.forEach(function(enemy) {
             enemy.render();
         });
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-        player.render();
-    }
-
-    /* This function does nothing but it could have been a good place to
-     * handle game reset states - maybe a new game menu or a game over screen
-     * those sorts of things. It's only called once by the init() method.
-     */
-    function reset() {
-        // noop
-    }
-=======
-=======
->>>>>>> master
         player.render();
         gems.render();
         nice.render();
     }
 
-<<<<<<< HEAD
->>>>>>> master
-=======
->>>>>>> master
 
     /* Go ahead and load all of the images we know we're going to need to
      * draw our game level. Then set init as the callback method, so that when
@@ -342,12 +220,6 @@ var Engine = (function(global) {
         'images/water-block.png',
         'images/grass-block.png',
         'images/enemy-bug.png',
-<<<<<<< HEAD
-<<<<<<< HEAD
-        'images/char-boy.png'
-=======
-=======
->>>>>>> master
         'images/char-boy.png',
         'images/Gem Blue.png',
         'images/Gem Green.png',
@@ -362,10 +234,6 @@ var Engine = (function(global) {
         'images/char-horn-girl.png',
         'images/arrow_up1.png',
         'images/game over.png'
-<<<<<<< HEAD
->>>>>>> master
-=======
->>>>>>> master
     ]);
     Resources.onReady(init);
 
@@ -374,18 +242,7 @@ var Engine = (function(global) {
      * from within their app.js files.
      */
     global.ctx = ctx;
-<<<<<<< HEAD
-<<<<<<< HEAD
-})(this);
-=======
     global.selected=selected;
 })(this);
 
 window.addEventListener('load', Engine);
->>>>>>> master
-=======
-    global.selected=selected;
-})(this);
-
-window.addEventListener('load', Engine);
->>>>>>> master
